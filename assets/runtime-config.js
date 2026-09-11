da2f75ce7d47fef9becff21903749a8acd2b5532
gmoon-code <graceyeunm@gmail.com>
2026-09-11 11:39:48 +0900

    Connect Research Chat to Cloudflare Worker

--- assets/runtime-config.js
+++ assets/runtime-config.js
@@ -1,6 +1,6 @@
 window.RMS_RUNTIME_CONFIG = Object.freeze({
-  version: '2.15.0',
-  freeEdition: true,
-  researchChatEndpoint: '',
-  chatEndpoint: ''
+  "version": "2.15.0",
+  "freeEdition": true,
+  "researchChatEndpoint": "https://rms-research-chat-free.gmoon-code.workers.dev/",
+  "chatEndpoint": "https://rms-research-chat-free.gmoon-code.workers.dev/"
 });
